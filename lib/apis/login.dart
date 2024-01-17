9499a16b5fa0e4580381802bdfe086904d9dc327
wangjun <444055828@qq.com>
2024-01-18 00:16:32 +0800

    savepoint

--- lib/apis/login.dart
+++ lib/apis/login.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 import 'dart:io';
 
+import 'package:flutter/material.dart';
 import 'package:webrtc_client/apis/common.dart';
 import 'package:http/http.dart';
 
@@ -9,10 +10,17 @@ class LoginReq {
   final String password;
 
   const LoginReq({required this.email, required this.password});
+
+  Map<String, dynamic> toJson() {
+    return {
+      "email": email,
+      "password": password,
+    };
+  }
 }
 
 Future<String> login({required String email, required String password}) async {
-  final resp = await put(Uri.parse("$baseURL/login"),
+  final resp = await post(Uri.parse("$baseURL/login"),
       headers: {"Content-Type": "application/json"},
       body: jsonEncode(LoginReq(email: email, password: password)));
   if (resp.statusCode != HttpStatus.ok) {
