3cf9167a82e8e1bc1ede0ba99c7b3e0b2462fc44
wangjun <444055828@qq.com>
2024-02-09 21:31:15 +0800

    savepoint

--- lib/webrtc.dart
+++ lib/webrtc.dart
@@ -5,16 +5,19 @@ import 'package:flutter_webrtc/flutter_webrtc.dart';
 import 'package:webrtc_client/main.dart';
 
 Future<RTCPeerConnection> createConnection(String peerID) async {
-  final peerConn = await createPeerConnection({
-    "iceServers": [
-      {"url": "stun:stun.l.google.com:19302"},
-    ]
-  }, {
-    "mandatory": {
-      "OfferToReceiveVideo": true,
+  final peerConn = await createPeerConnection(
+    {
+      "iceServers": [
+        {"url": "stun:stun.l.google.com:19302"},
+      ]
     },
-    "optional": [],
-  });
+    // {
+    //   "mandatory": {
+    //     "OfferToReceiveVideo": true,
+    //   },
+    //   "optional": [],
+    // }
+  );
   peerConn.onIceCandidate = (candidate) {
     debugPrint("onIceCandidate: ${candidate.sdpMLineIndex}");
     WS.getOrCreateSink(AuthToken.token).add(jsonEncode({
